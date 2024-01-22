5ac587303bcac2a6b15f9d9f9a76ffcd90a5935d
anakon73 <nazarkinah73@gmail.com>
2024-01-22 14:44:09 +0200

    feat(SStar): implement component

---
+++ src/shared/ui/SStar/SStar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { StarIcon } from '@heroicons/vue/16/solid'
+
+export interface Props {
+  active?: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <StarIcon
+    class="size-4 text-blue-600"
+    :class="active ? 'opacity-100' : 'opacity-60'"
+  />
+</template>
---
+++ src/shared/ui/SStar/index.ts
@@ -0,0 +1,1 @@
+export { default as SStar, type Props as SStarProps } from './SStar.vue'
