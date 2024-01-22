66f53f6456222b97abd31ea43df59f97bd121410
anakon73 <nazarkinah73@gmail.com>
2024-01-22 15:50:10 +0200

    feat(SRating): implement component

---
+++ src/entities/Product/ui/SRating/SRating.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { IRating } from '@/shared/types'
+import { SStar } from '@/shared/ui/SStar'
+
+export interface Props {
+  rating: IRating
+  quantity?: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex items-center">
+    <SStar :active="rating.rate >= 0.5" />
+    <SStar
+      class="-ml-1"
+      :active="rating.rate >= 1.5"
+    />
+    <SStar
+      class="-ml-1"
+      :active="rating.rate >= 2.5"
+    />
+    <SStar
+      class="-ml-1"
+      :active="rating.rate >= 3.5"
+    />
+    <SStar
+      class="-ml-1"
+      :active="rating.rate >= 4.5"
+    />
+
+    <p
+      v-if="quantity"
+      aria-label="quantity"
+      class="whitespace-nowrap text-[10px] lg:text-xs"
+    >
+      <span class="font-medium leading-5 text-blue-600">{{
+        rating.rate
+      }}</span>
+      ({{ rating.quantity }}
+      відгуків)
+    </p>
+  </div>
+</template>
---
+++ src/entities/Product/ui/SRating/index.ts
@@ -0,0 +1,1 @@
+export { default as SRating, type Props as SRatingProps } from './SRating.vue'
