bafa627a8c9845d19b5b0f0efc8b3c84d375cac6
anakon73 <nazarkinah73@gmail.com>
2024-01-22 14:50:29 +0200

    test(SStar): add tests for render component & \active\ prop

---
+++ src/shared/ui/SStar/SStar.test.ts
@@ -0,0 +1,17 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest'
+import { SStar } from '.'
+
+describe('SStar', () => {
+  test('Should render component not active', () => {
+    const wrapper = mount(SStar)
+
+    expect(wrapper.find('[data-slot="icon"]').exists()).toBeTruthy()
+  })
+
+  test('Should render component active', () => {
+    const wrapper = mount(SStar, { props: { active: true } })
+
+    expect(wrapper.element.classList.contains('opacity-100')).toBeTruthy()
+  })
+})
