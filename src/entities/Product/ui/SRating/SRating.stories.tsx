d4d66a3c3d3be0a8dd94c4bdc6dcc764a0017ab2
anakon73 <nazarkinah73@gmail.com>
2024-01-22 15:50:19 +0200

    docs(SRating): describe stories

---
+++ src/entities/Product/ui/SRating/SRating.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import { SRating } from '.'
+
+export default {
+  title: 'Entities/Product/SRating',
+  component: SRating,
+  tags: ['autodocs'],
+
+  argTypes: {
+    quantity: { type: 'boolean' },
+  },
+
+  args: {
+    quantity: false,
+    rating: {
+      quantity: 127,
+      rate: 4.8,
+    },
+  },
+} satisfies Meta<typeof SRating>
+
+type Story = StoryObj<typeof SRating>
+
+export const Default: Story = {}
+
+export const WithQuantity: Story = { args: { quantity: true } }
