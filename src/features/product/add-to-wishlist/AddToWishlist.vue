e711c0ab74363bdeeb0741b119ab73cacc0ed959
anakon73 <nazarkinah73@gmail.com>
2024-01-26 15:50:33 +0200

    feat(AddToWishlist): implement component

---
+++ src/features/product/add-to-wishlist/AddToWishlist.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { type ButtonHTMLAttributes, computed, toRefs } from 'vue'
+import { HeartIcon } from '@heroicons/vue/24/outline'
+import { HeartIcon as HeartIconSolid } from '@heroicons/vue/24/solid'
+
+import { cn } from '@/shared/lib/styles'
+
+export interface Props extends /* @vue-ignore */ ButtonHTMLAttributes {
+  liked: boolean
+}
+
+export type Emits = {
+  like: []
+}
+
+const props = defineProps<Props>()
+
+const { liked } = toRefs(props)
+
+const emits = defineEmits<Emits>()
+
+const computedIcon = computed(() => {
+  return liked.value ? HeartIconSolid : HeartIcon
+})
+</script>
+
+<template>
+  <button
+    :class="cn(
+      'p-1.5 rounded-full transition-colors duration-300',
+      liked
+        ? 'text-white bg-blue-600 hover:text-zinc-100 hover:bg-blue-700'
+        : 'text-blue-600 bg-white hover:text-blue-700 hover:bg-zinc-100',
+    )"
+    @click="emits('like')"
+  >
+    <component :is="computedIcon" class="size-3" />
+  </button>
+</template>
---
+++ src/features/product/add-to-wishlist/index.ts
@@ -0,0 +1,5 @@
+export {
+  default as AddToWishlist,
+  type Props as AddToWishlistProps,
+  type Emits as AddToWishlistEmits,
+} from './AddToWishlist.vue'
