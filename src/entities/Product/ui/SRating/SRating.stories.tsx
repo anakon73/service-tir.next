7b636beb7f937270f886a8723d074d154677394d
anakon73 <nazarkinah73@gmail.com>
2024-02-07 20:21:34 +0200

    docs(SRating): story title

--- src/entities/Product/ui/SRating/SRating.stories.tsx
+++ src/entities/Product/ui/SRating/SRating.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import { SRating } from '.'
 
 export default {
-  title: 'Entities/Product/SRating',
+  title: 'Entities/Product/UI/SRating',
   component: SRating,
   tags: ['autodocs'],
 
