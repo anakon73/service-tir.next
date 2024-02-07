4bf3ff2f30507cb1a065c3672f43a3d24bca0c5e
anakon73 <nazarkinah73@gmail.com>
2024-02-07 20:09:52 +0200

    docs(AdvantageCard): describe stories

---
+++ src/widgets/AdvantageCard/AdvantageCard.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { AdvantageCard, type AdvantageCardProps } from '.'
+
+const meta = {
+  title: 'Widgets/AdvantageCard',
+  component: AdvantageCard,
+} satisfies Meta<AdvantageCardProps>
+
+export default meta
+
+type Story = StoryObj<AdvantageCardProps>
+
+export const Default: Story = {
+  args: {
+    name: 'Швидкість роботи',
+    description: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc
+      ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus
+      amet etiam tincidunt.
+    `,
+    image: '🚀',
+  },
+}
