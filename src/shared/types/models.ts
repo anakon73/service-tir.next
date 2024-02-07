1b0083d0349e216e94cee273683bdcd2a4e4bbe1
anakon73 <nazarkinah73@gmail.com>
2024-02-07 19:53:16 +0200

    feat(types): Product

--- src/shared/types/models.ts
+++ src/shared/types/models.ts
@@ -1,4 +1,14 @@
-export type IRating = {
+export interface Rating {
   rate: number
   quantity: number
+}
+
+export interface Product {
+  name: string
+  description: string
+  discount: boolean
+  rate: Rating
+  image: string
+  price: number
+  priceWithDiscount?: number
 }
