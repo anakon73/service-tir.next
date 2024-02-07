f385ff4ad4ae6ac3b72262c8a6d910bd3edecef6
anakon73 <nazarkinah73@gmail.com>
2024-02-07 20:21:58 +0200

    docs(SSearchItem): describe stories

---
+++ src/entities/Product/ui/SSearchItem/SSearchItem.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { SSearchItem, type SSearchItemProps } from '.'
+
+const meta = {
+  title: 'Entities/Product/UI/SSearchItem',
+  component: SSearchItem,
+} satisfies Meta<SSearchItemProps>
+
+export default meta
+
+type Story = StoryObj<SSearchItemProps>
+
+export const Default: Story = {
+  args: {
+    id: 6422889,
+    image: `
+    https://e7.pngegg.com/pngimages/856/952/png-clipart-
+fuel-tank-diesel-fuel-storage-tank-petroleum-truck-rectangle-truck.png`,
+    name: 'Паливний бак Scania',
+    price: 2200,
+    rate: {
+      quantity: 127,
+      rate: 4.8,
+    },
+  },
+}
