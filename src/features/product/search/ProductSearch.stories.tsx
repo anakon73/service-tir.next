09a24e563bba98969eefea6bf874e32ee598c1c4
anakon73 <nazarkinah73@gmail.com>
2024-02-07 20:48:56 +0200

    docs(ProductSearch): describe stories

---
+++ src/features/product/search/ProductSearch.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { ProductSearch } from '.'
+
+const meta = {
+  title: 'Features/Product/Search',
+  component: ProductSearch,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<{ mobile: boolean }>
+
+export default meta
+
+type Story = StoryObj<{ mobile: boolean }>
+
+export const Default: Story = {}
+
+export const Mobile: Story = { args: { mobile: true } }
