7f61e8e61c618849ba7a857cf137eba70ae4c1bf
anakon <nazarkinah73@gmail.com>
2024-03-01 20:48:45 +0200

    build(happy-dom): install

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [
     vue(),
     VueJsx(),
   ],
+  test: {
+    environment: 'happy-dom',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
