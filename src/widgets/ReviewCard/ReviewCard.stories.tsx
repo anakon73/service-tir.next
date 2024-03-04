a95cb399db0ae4066fb31398a1a12c61ad26bff3
anakon <nazarkinah73@gmail.com>
2024-03-04 16:19:10 +0200

    docs(ReviewCard): add args

--- src/widgets/ReviewCard/ReviewCard.stories.tsx
+++ src/widgets/ReviewCard/ReviewCard.stories.tsx
@@ -4,6 +4,24 @@ import { ReviewCard } from '.'
 export default {
   title: 'Widgets/ReviewCard',
   component: ReviewCard,
+
+  args: {
+    author: 'Ann Lubin',
+    comment: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc
+      ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet
+      etiam tincidunt rhoncus, ullamcorper velit. Ullamcorper risus tempor, ac
+      nunc libero urna, feugiat. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam
+      lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.
+      Ullamcorper risus tempor, ac nunc libero urna, feugiat.
+    `,
+    productName: 'Диск гальмівний Scania',
+    rate: {
+      quantity: 127,
+      rate: 4.7,
+    },
+  },
 } satisfies Meta<typeof ReviewCard>
 
 type Story = StoryObj<typeof ReviewCard>
