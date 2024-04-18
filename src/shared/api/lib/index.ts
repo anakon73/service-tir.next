5dd5c40211e2698c1eb31959de9540e6ccd5fdb4
anakon <nazarkinah73@gmail.com>
2024-04-18 13:29:24 +0300

    feat(api/lib): types for queryKeys & refs checker

--- src/shared/api/lib/index.ts
+++ src/shared/api/lib/index.ts
@@ -1,3 +1,5 @@
+import type { MaybeRef } from 'vue'
+import { and } from '@vueuse/math'
 import type { z } from 'zod'
 
 // Endpoints
@@ -8,3 +10,12 @@ type EndpointAndSchema = {
 }
 
 export type ApiEndpointsAndSchemas = Record<string, EndpointAndSchema>
+
+// Query keys
+type KeyParam<T = number> = MaybeRef<T | null | undefined>
+
+export type ToKeyParams<T extends Record<string, unknown>> = {
+  [K in keyof T]: KeyParam<T[K]>
+}
+
+export const paramsAnd = (params: Record<string, KeyParam<unknown>>) => and(...Object.values(params))
