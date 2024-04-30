dfb166ddc7fbf891e57f164ad9663fc7f1601a93
anakon <nazarkinah73@gmail.com>
2024-04-30 13:12:09 +0300

    feat(review): impl mock handlers

--- src/shared/api/handlers.ts
+++ src/shared/api/handlers.ts
@@ -1,7 +1,9 @@
 import { articlesHandlers } from './article/mock'
 import { productsHandlers } from './product/mock'
+import { reviewHandlers } from './review/mock'
 
 export const handlers = [
   ...productsHandlers,
   ...articlesHandlers,
+  ...reviewHandlers,
 ]
---
+++ src/shared/api/review/mock.ts
@@ -0,0 +1,32 @@
+import type { z } from 'zod'
+import { fakerUK as f } from '@faker-js/faker'
+import { HttpResponse, http } from 'msw'
+
+import type { Review } from '@/shared/types'
+
+import type { ReviewSchema } from './types'
+import { normalizeReview } from './normalizers'
+import { SuccessfulResponseMock } from '../lib'
+
+export function makeReviewSchemaMock(): z.infer<typeof ReviewSchema> {
+  return {
+    author: f.person.fullName(),
+    product_name: f.lorem.words({ min: 1, max: 4 }),
+    comment: f.lorem.words({ min: 40, max: 68 }),
+    rate: {
+      quantity: f.number.int({ max: 130 }),
+      rate: f.number.float({ min: 0.5, max: 5, fractionDigits: 1 }),
+    },
+  }
+}
+
+export const makeReviewMock = (): Review => normalizeReview(makeReviewSchemaMock())
+
+export const reviewHandlers = [
+  http.get('/api/reviews', () => {
+    return HttpResponse.json(Array.from({ length: 37 }, () => makeReviewSchemaMock()))
+  }),
+  http.post('/api/reviews', () => {
+    return HttpResponse.json(SuccessfulResponseMock)
+  }),
+]
