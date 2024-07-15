da4f4ccf1f07f0fb85ff59e2fee5bbe60f5001c4
anakon <nazarkinah73@gmail.com>
2024-07-15 14:40:48 +0300

    feat: impl layouts & params from url

--- src/pages/blog/BlogPage.vue
+++ src/pages/blog/BlogPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
 import { format } from 'date-fns'
 import { Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -8,11 +9,8 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
-import { SHeader } from '@/widgets/header'
 import { SArticleCard } from '@/entities/article'
-
 import { useArticleById } from '@/shared/api/article'
-import { SFooter } from '@/shared/ui/SFooter'
 
 const modules = [Navigation, Pagination]
 
@@ -21,144 +19,139 @@ const pagination = ref(null)
 const prev = ref(null)
 const next = ref(null)
 
-const { data, isLoading } = useArticleById({ id: 1 })
+const { params: { id } } = useRoute()
+
+const { data, isLoading } = useArticleById({ id: +id || 1 })
 
 const formattedDate = computed(() => format(data.value!.date, 'd MMMM u'))
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col justify-between">
-    <div class="basis-full">
-      <SHeader class="mb-10 md:mb-5" />
+  <div class="container">
+    <div
+      v-if="isLoading"
+      class="w-full text-center text-3xl font-bold"
+    >
+      Is Loading...
+    </div>
 
-      <div class="container">
+    <div
+      v-else-if="data === undefined"
+      class="w-full text-center text-3xl font-bold"
+    >
+      Not Found Blog
+    </div>
+
+    <div v-else>
+      <div
+        class="
+        mb-6 flex items-center gap-1 text-xs text-gray-900
+        transition-colors duration-300 hover:text-gray-950 md:mb-12
+        "
+      >
+        <RouterLink to="/" class="opacity-50">
+          Головна
+        </RouterLink>
+        <ChevronRightIcon class="size-2" />
+        <RouterLink to="/articles" class="opacity-50">
+          Статті
+        </RouterLink>
+        <ChevronRightIcon class="size-2" />
+        <p class="cursor-pointer">
+          {{ data.name }}
+        </p>
+      </div>
+      <div class="mb-[87px]">
+        <p class="mb-4 text-xs font-semibold leading-5 text-gray-400">
+          {{ formattedDate }}
+        </p>
+        <h1 class="mb-6 text-3xl font-semibold text-zinc-900 lg:text-big">
+          {{ data.name }}
+        </h1>
         <div
-          v-if="isLoading"
-          class="w-full text-center text-3xl font-bold"
-        >
-          Is Loading...
-        </div>
 
-        <div
-          v-else-if="data === undefined"
-          class="w-full text-center text-3xl font-bold"
+          class="flex w-full flex-col gap-7"
         >
-          Not Found Blog
-        </div>
-
-        <div v-else>
           <div
-            class="
-            mb-6 flex items-center gap-1 text-xs text-gray-900
-            transition-colors duration-300 hover:text-gray-950 md:mb-12
-            "
+            v-for="({ image, text }, index) in data.paragraphs"
+            :key="index"
+            class="flex flex-col gap-7"
           >
-            <p class="cursor-pointer opacity-50">
-              Головна
-            </p>
-            <ChevronRightIcon class="size-2" />
-            <p class="cursor-pointer opacity-50">
-              Статті
-            </p>
-            <ChevronRightIcon class="size-2" />
-            <p class="cursor-pointer">
-              {{ data.name }}
-            </p>
-          </div>
-          <div class="mb-[87px]">
-            <p class="mb-4 text-xs font-semibold leading-5 text-gray-400">
-              {{ formattedDate }}
-            </p>
-            <h1 class="mb-6 text-3xl font-semibold text-zinc-900 lg:text-big">
-              {{ data.name }}
-            </h1>
+            <img
+              class="rounded-lg "
+              :src="image"
+              :alt="`paragraph ${index} image`"
+            >
             <div
-
-              class="flex w-full flex-col gap-7"
+              class="
+              flex flex-col gap-7 text-xs leading-5 text-gray-900
+              md:text-sm lg:text-base lg:leading-6
+              "
             >
-              <div
-                v-for="({ image, text }, index) in data.paragraphs"
-                :key="index"
-                class="flex flex-col gap-7"
-              >
-                <img
-                  class="rounded-lg "
-                  :src="image"
-                  :alt="`paragraph ${index} image`"
-                >
-                <div
-                  class="
-                  flex flex-col gap-7 text-xs leading-5 text-gray-900
-                  md:text-sm lg:text-base lg:leading-6
-                  "
-                >
-                  <p v-for="(t, i) in text" :key="i">
-                    {{ t }}
-                  </p>
-                </div>
-              </div>
+              <p v-for="(t, i) in text" :key="i">
+                {{ t }}
+              </p>
             </div>
-          </div>
-        </div>
-        <div class="-mx-5 mb-[84px] overflow-hidden p-5">
-          <div class="mb-9 flex items-center justify-between">
-            <div class="flex items-center gap-3">
-              <div class="paragraphIcon size-10 rounded-full bg-white p-2 text-lg">
-                📝
-              </div>
-              <div
-                class="font-jakarta text-3xl font-bold leading-normal text-gray-900 lg:text-big lg:leading-extra-height"
-              >
-                Схожі статті
-              </div>
-            </div>
-          </div>
-          <Swiper
-            :breakpoints="{
-              400: { slidesPerView: 2 },
-              768: { slidesPerView: 3 },
-            }"
-            :pagination="{
-              el: pagination,
-              clickable: true,
-            }"
-            :modules="modules"
-            :navigation="{
-              prevEl: prev,
-              nextEl: next,
-            }"
-            :slides-per-view="1"
-            loop
-            :space-between="20"
-          >
-            <SwiperSlide v-for="similar in data?.similarArticles" :key="similar.id">
-              <SArticleCard
-                :date="similar.date"
-                :image="similar.previewImage"
-                :description="similar.description"
-                :name="similar.name"
-              />
-            </SwiperSlide>
-          </Swiper>
-          <div class="mt-5 flex items-end justify-center gap-5">
-            <button
-              ref="prev"
-              class="paragraphIcon rounded-full bg-white p-2 transition-all duration-300 hover:scale-105 hover:bg-zinc-50"
-            >
-              <ChevronLeftIcon class="size-6" />
-            </button>
-            <div ref="pagination" class="!relative !w-auto" />
-            <button
-              ref="next"
-              class="paragraphIcon rounded-full bg-white p-2 transition-all duration-300 hover:scale-105 hover:bg-zinc-50"
-            >
-              <ChevronRightIcon class="size-6" />
-            </button>
           </div>
         </div>
       </div>
     </div>
-
-    <SFooter />
+    <div class="-mx-5 mb-[84px] overflow-hidden p-5">
+      <div class="mb-9 flex items-center justify-between">
+        <div class="flex items-center gap-3">
+          <div class="paragraphIcon size-10 rounded-full bg-white p-2 text-lg">
+            📝
+          </div>
+          <div
+            class="font-jakarta text-3xl font-bold leading-normal text-gray-900 lg:text-big lg:leading-extra-height"
+          >
+            Схожі статті
+          </div>
+        </div>
+      </div>
+      <Swiper
+        :breakpoints="{
+          400: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+        }"
+        :pagination="{
+          el: pagination,
+          clickable: true,
+        }"
+        :modules="modules"
+        :navigation="{
+          prevEl: prev,
+          nextEl: next,
+        }"
+        :slides-per-view="1"
+        loop
+        :space-between="20"
+      >
+        <SwiperSlide v-for="similar in data?.similarArticles" :key="similar.id">
+          <SArticleCard
+            :id="similar.id"
+            :date="similar.date"
+            :image="similar.previewImage"
+            :description="similar.description"
+            :name="similar.name"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div class="mt-5 flex items-end justify-center gap-5">
+        <button
+          ref="prev"
+          class="paragraphIcon rounded-full bg-white p-2 transition-all duration-300 hover:scale-105 hover:bg-zinc-50"
+        >
+          <ChevronLeftIcon class="size-6" />
+        </button>
+        <div ref="pagination" class="!relative !w-auto" />
+        <button
+          ref="next"
+          class="paragraphIcon rounded-full bg-white p-2 transition-all duration-300 hover:scale-105 hover:bg-zinc-50"
+        >
+          <ChevronRightIcon class="size-6" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
