b54ee4123a993a188d4e2660e3900fbac2d9417a
anakon <nazarkinah73@gmail.com>
2024-07-15 14:36:58 +0300

    feat(Layouts): impl

---
+++ src/pages/layouts/AuthLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container xl:pr-20 2xl:pr-0">
+    <RouterLink to="/">
+      <img
+        class="absolute top-4"
+        src="/src/shared/assets/images/newLogo.svg"
+        alt="service tir logo"
+      >
+    </RouterLink>
+    <div
+      class="
+      mb-2 mt-28 flex flex-col items-center gap-12
+      lg:my-0 lg:flex-row lg:justify-between lg:gap-0
+      "
+    >
+      <slot />
+      <img
+        class="w-full lg:h-screen lg:w-auto lg:py-4 lg:pl-4"
+        src="/src/shared/assets/images/forestPhoto.png"
+        alt="forest photo"
+      >
+    </div>
+  </div>
+</template>
---
+++ src/pages/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SHeader } from '@/widgets/header'
+import { SFooter } from '@/shared/ui/SFooter'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col justify-between">
+    <div>
+      <SHeader class="relative z-50 mb-10 md:mb-5" />
+      <slot />
+    </div>
+    <SFooter />
+  </div>
+</template>
