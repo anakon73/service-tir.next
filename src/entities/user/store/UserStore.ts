7421c0cbc318ab74339c6f2389e6cd80749bba89
anakon <nazarkinah73@gmail.com>
2024-09-16 15:11:15 +0300

    feat(UserStore): cart

--- src/entities/user/store/UserStore.ts
+++ src/entities/user/store/UserStore.ts
@@ -1,7 +1,10 @@
+import { computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
 import type { User } from '@/shared/types'
+import { useCart } from '@/shared/api/cart'
+import { useProducts } from '@/shared/api/product'
 
 export const useUserStore = defineStore('user', () => {
   const user = useLocalStorage<User | null>(
@@ -28,9 +31,58 @@ export const useUserStore = defineStore('user', () => {
     user.value = null
   }
 
+  const { data: cart } = useCart()
+  const { data: products } = useProducts()
+
+  const cartList = computed(() => {
+    return products.value
+      ?.filter(product => cart.value?.some(item => item.productCode === product.code))
+      .map((product) => {
+        const cartItem = cart.value?.find(item => item.productCode === product.code)
+
+        return {
+          ...product,
+          count: cartItem?.count ?? 1,
+        }
+      })
+  })
+
+  const price = computed(() => {
+    return cartList.value?.reduce((total, item) => total + item.price, 0) ?? 0
+  })
+
+  const totalDiscount = computed(() => {
+    return cartList.value?.reduce((total, item) => {
+      if (item.priceWithDiscount !== null) {
+        return total + (item.price - item.priceWithDiscount)
+      }
+      return total
+    }, 0) ?? 0
+  })
+
+  const totalPrice = computed(() => {
+    return cartList.value?.reduce((total, item) => {
+      return total + (
+        item.priceWithDiscount !== null
+          ? item.priceWithDiscount
+          : item.price
+      )
+    }, 0) ?? 0
+  })
+
   function setEmail(email: string | null) {
     emailForResetPassword.value = email
   }
 
-  return { user, setUser, logout, emailForResetPassword, setEmail }
+  return {
+    user,
+    setUser,
+    logout,
+    emailForResetPassword,
+    setEmail,
+    totalPrice,
+    price,
+    cartList,
+    totalDiscount,
+  }
 })
