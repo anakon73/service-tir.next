4f4d1c4deaca0581d245a74368a431fdd73ca462
anakon <nazarkinah73@gmail.com>
2024-09-16 16:31:58 +0300

    feat(router): def pages without auth

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/pages/routes'
 import { loadLayoutMiddleware } from '@/shared/lib/router'
+import { useUserStore } from '@/entities/user'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -10,6 +11,20 @@ export const router = createRouter({
       top: 0,
     }
   },
+})
+
+router.beforeEach((to, _from, next) => {
+  const userStore = useUserStore()
+
+  if (
+    (to.name?.startsWith('Profile') || to.name === 'Cart' || to.name === 'Order')
+    && !userStore.user
+  ) {
+    next({ name: 'Register' })
+  }
+  else {
+    next()
+  }
 })
 
 router.beforeEach(loadLayoutMiddleware)
