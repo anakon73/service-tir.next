1421938fd2abe8ae82cb4a1a82d4709556c76879
anakon <nazarkinah73@gmail.com>
2024-11-17 11:27:39 +0200

    docs(SServiceCard): format: & id arg

--- src/entities/service/ui/SServiceCard/SServiceCard.stories.tsx
+++ src/entities/service/ui/SServiceCard/SServiceCard.stories.tsx
@@ -1,25 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import { SServiceCard, type SServiceCardProps } from '.'
 
-export default {
+const meta = {
   title: 'Entities/Service/UI/SServiceCard',
   component: SServiceCard,
 
   args: {
+    id: 1,
     name: 'Дезинфекция и озонирование автомобилей и автобусов',
     description: `
       Это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem
       Ipsum является
     `,
-
     image: 'https://media.istockphoto.com/id/1287044692/photo/worker-washing-red-car-with-sponge-on-a-car-wash.jpg?s=612x612&w=0&k=20&c=_6WO9k1qkDub5CAEQgnORMduUoQJkU6w3jjVQTdTdwQ=',
-  },
-
-  argTypes: {
-    image: { table: { disable: true } },
   },
 } satisfies Meta<SServiceCardProps>
 
-type Story = StoryObj<SServiceCardProps>
+export default meta
 
-export const Default: Story = { name: 'SServiceCard' }
+export const Default: StoryObj<typeof meta> = { name: 'SServiceCard' }
