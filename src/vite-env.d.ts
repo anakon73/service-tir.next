da5df848e1832062b1810bbf2d8885a97649bd07
anakon <nazarkinah73@gmail.com>
2024-11-17 11:35:52 +0200

    feat(vite-env): remove Error type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-import '@tanstack/vue-query'
-import type { FetchError } from './shared/api/lib'
-
-declare module '@tanstack/vue-query' {
-  interface Register {
-    defaultError: FetchError
-  }
-}
