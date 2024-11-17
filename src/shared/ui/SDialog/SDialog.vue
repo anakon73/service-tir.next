806ad1e37a2fe6d833bf37e71c5ad27fc44d0c57
anakon <nazarkinah73@gmail.com>
2024-11-17 11:22:07 +0200

    refactor(SDialog): use radix-vue

--- src/shared/ui/SDialog/SDialog.vue
+++ src/shared/ui/SDialog/SDialog.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import {
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+} from 'radix-vue'
 import { onClickOutside, onKeyDown } from '@vueuse/core'
-import { Dialog, DialogOverlay, DialogPanel } from '@headlessui/vue'
 
 import { type ClassValue, cn } from '@/shared/lib/styles'
 import type { Size } from './types'
@@ -18,41 +23,41 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emits = defineEmits<{ close: [] }>()
 
-const panel = ref(null)
+const dialogContent = ref(null)
 
 onKeyDown('Escape', () => emits('close'))
 
-onClickOutside(panel, () => emits('close'))
+onClickOutside(dialogContent, () => emits('close'))
 </script>
 
 <template>
-  <Dialog as="div" :open="open" class="relative z-50">
-    <DialogOverlay
-      class="fixed inset-0 z-40 bg-black/25"
-    />
-
-    <div
-      class="
-        fixed inset-0 z-50 flex h-full items-center justify-center overflow-y-auto p-5
-        text-center
-      "
-    >
-      <DialogPanel
-        ref="panel"
-        :class="
-          cn(
+  <DialogRoot :open="open">
+    <DialogPortal>
+      <DialogOverlay
+        class="fixed inset-0 z-50 bg-black/25"
+        @click="emits('close')"
+      />
+      <DialogContent
+        class="
+          fixed inset-0 z-50 flex h-full items-center justify-center
+          overflow-y-auto p-5 text-center
+        "
+      >
+        <div
+          ref="dialogContent"
+          :class="cn(
             `
-              relative w-full overflow-hidden rounded-[30px] bg-white py-10 text-left
-              align-middle transition-all
+              relative w-full overflow-hidden rounded-[30px] bg-white py-10
+              text-left align-middle transition-all
             `,
             size === 'md' && 'max-w-md',
             size === 'lg' && 'max-w-[600px]',
             props.class,
-          )
-        "
-      >
-        <slot />
-      </DialogPanel>
-    </div>
-  </Dialog>
+          )"
+        >
+          <slot />
+        </div>
+      </DialogContent>
+    </DialogPortal>
+  </DialogRoot>
 </template>
