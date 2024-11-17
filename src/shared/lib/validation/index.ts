77d2402e0ef09b0170210dfc34f1f199e6ab7d8e
anakon <nazarkinah73@gmail.com>
2024-11-17 11:24:49 +0200

    feat(lib/validation): dateValidation & PaginatedResponseSchema

--- src/shared/lib/validation/index.ts
+++ src/shared/lib/validation/index.ts
@@ -1,6 +1,23 @@
-import type { z } from 'zod'
+import { z } from 'zod'
 import type { AnyObject } from '@/shared/lib/utils'
 
 export type ValidationSchema<T extends AnyObject> = {
   [K in keyof T]: z.ZodType<T[K]>
+}
+
+export const dateValidation = z.preprocess(
+  arg => (typeof arg === 'string' ? new Date(arg) : arg),
+  z.date(),
+) as z.ZodEffects<z.ZodDate, Date>
+
+export function PaginatedResponseSchema<ItemType extends z.ZodTypeAny>(
+  itemSchema: ItemType,
+) {
+  return z.object({
+    data: z.array(itemSchema),
+    current_page: z.number(),
+    per_page: z.number(),
+    total: z.number(),
+    last_page: z.number(),
+  })
 }
