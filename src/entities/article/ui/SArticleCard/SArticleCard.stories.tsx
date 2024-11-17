ac79b5ce4fae403c4e405d0242f36760c9bec075
anakon <nazarkinah73@gmail.com>
2024-11-17 11:26:33 +0200

    docs(SArticle): rename args & format

--- src/entities/article/ui/SArticleCard/SArticleCard.stories.tsx
+++ src/entities/article/ui/SArticleCard/SArticleCard.stories.tsx
@@ -6,21 +6,15 @@ export default {
   component: SArticleCard,
 
   args: {
-    date: new Date('September 1, 2023'),
+    id: 1,
+    createdAt: new Date('September 1, 2023'),
     name: 'Як відбувається ремонт двигунів?',
-    description:
-      // eslint-disable-next-line max-len
-      'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
-    image:
-      // eslint-disable-next-line max-len
-      'https://i0.wp.com/allproservicenter.com/wp-content/uploads/2021/02/engine-repair-300x200-1.jpg?resize=270%2C180&ssl=1',
+    description: 'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    previewImage: 'https://i0.wp.com/allproservicenter.com/wp-content/uploads/2021/02/engine-repair-300x200-1.jpg?resize=270%2C180&ssl=1',
   },
 
   argTypes: {
-    date: { control: 'date' },
-    name: { control: 'text' },
-    description: { control: 'text' },
-    image: { control: 'text' },
+    createdAt: { control: 'date' },
   },
 } satisfies Meta<typeof SArticleCard>
 
